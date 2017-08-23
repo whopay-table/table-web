2edb1e67ba224806f19a742c4c82f2a9706025e6
Jeongmin Byun <jmbyun91@gmail.com>
2017-08-24 07:36:23 +0900

    feat(package): improve NotFound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router-dom';
+import Header from './Header';
 
 export default class NotFound extends Component {
   static propTypes = {};
@@ -11,9 +12,23 @@ export default class NotFound extends Component {
 
   render() {
     return (
-      <div className="c-not-found">
+      <div className="c-not-found u-header-container">
+        <Header />
         <div className="u-container">
-          This is not the place you're looking for.
+          <div className="c-not-found__body">
+            <i className="fa fa-exclamation-triangle" />
+            &nbsp;
+            알 수 없는 주소<br />
+            <br />
+            여기엔 보여드릴 정보가 없습니다.<br />
+            <br />
+            <Link
+              to="/"
+              className="u-button u-button--left-align"
+            >
+              메인화면으로 돌아가기
+            </Link>
+          </div>
         </div>
       </div>
     )
