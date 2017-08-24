aed20e90f957945052b0eeeade4fad2fb09daf25
Jeongmin Byun <jmbyun91@gmail.com>
2017-08-24 11:38:56 +0900

    fix(package): fix index page link from Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router-dom';
+import * as Server from '../../configs/server';
+
+const HOSTNAME = window.location.hostname;
+const DOMAIN = Server.domain;
+const IS_PROD_DOMAIN = HOSTNAME.endsWith(DOMAIN);
 
 export default class Header extends Component {
   static propTypes = {
@@ -7,21 +12,33 @@ export default class Header extends Component {
   };
 
   render() {
+    const logoBody = (
+      <div className="c-group-header__logo-block">
+        <img
+          src="/image/colored-logo.png"
+          className="c-header__logo"
+        />
+        <div className="c-header__title">
+          <span className="c-header__app-title">
+            WHOPAY
+          </span>
+        </div>
+      </div>
+    );
+
+    const logo = IS_PROD_DOMAIN ? (
+      <a href={`//${DOMAIN}`}>
+        {logoBody}
+      </a>
+    ) : (
+      <Link to="/">
+        {logoBody}
+      </Link>
+    );
+
     return (
       <div className="c-header">
-        <Link to="/">
-          <div className="c-group-header__logo-block">
-            <img
-              src="/image/colored-logo.png"
-              className="c-header__logo"
-            />
-            <div className="c-header__title">
-              <span className="c-header__app-title">
-                WHOPAY
-              </span>
-            </div>
-          </div>
-        </Link>
+        {logo}
       </div>
     );
   }
