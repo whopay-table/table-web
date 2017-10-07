38bddf4a4ebc39cb6ce19dce3031de7a3dcda969
Jeongmin Byun <jmbyun91@gmail.com>
2017-10-07 16:50:41 +0900

    feat(package): show user absolute date for date older than 7 days

--- src/components/TimeAgo.js
+++ src/components/TimeAgo.js
@@ -1,12 +1,33 @@
+import moment from 'moment';
 import React, { Component, PropTypes } from 'react';
-import TimeAgo from 'react-timeago';
+import ReactTimeAgo from 'react-timeago';
 import TimeAgoKoStrings from 'react-timeago/lib/language-strings/ko';
 import TimeAgoBuildFormatter from 'react-timeago/lib/formatters/buildFormatter'
 
 const formatter = TimeAgoBuildFormatter(TimeAgoKoStrings);
+moment.locale('ko');
 
-export default class TableTimeAgo extends Component {
+export default class TimeAgo extends Component {
+  renderDate() {
+    const { date } = this.props;
+    const dateString = moment(date).format('L');
+    return (
+      <span className="c-timeago c-timeago--datestring">
+        {dateString}
+      </span>
+    );
+  }
+
+  renderTimeAgo() {
+    return <ReactTimeAgo {...this.props} formatter={formatter} />;
+  }
+
   render() {
-    return <TimeAgo {...this.props} formatter={formatter} />;
+    const { date } = this.props;
+    if (moment().diff(moment(date), 'days') >= 7) {
+      return this.renderDate();
+    } else {
+      return this.renderTimeAgo();
+    }
   }
 }
