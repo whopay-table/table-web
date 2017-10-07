5af091f30599183b0a7e46c4c35292e24ef2d838
Jeongmin Byun <jmbyun91@gmail.com>
2017-10-07 15:22:46 +0900

    fix(package): login page now allows user to submit form with return key

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -90,7 +90,7 @@ export default class LoginForm extends Component {
         </div>
         {formGroups}
         {alertBlock}
-        <input type="submit" className="u-no-display" />
+        <input type="submit" className="u-hidden-submit" />
         <div className="u-button-row">
           <a
             className="u-button"
